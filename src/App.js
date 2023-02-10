d3785ac3957c524fb0fc249b43c618b603c70130
Jacob Showman <jacob.showman@mrjaffesclass.com>
2023-02-10 12:47:07 -0800

    Working with Fetch

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let jsondata;    
+    console.log (jsondata)
+    fetch('https://us-central1-big-bang-theory-25fd5.cloudfunctions.net/bbt361642/bbt/episodes').then(
+            function(u){ return u.json();}
+          ).then(
+            function(json){
+              jsondata = json;
+            }
+          )
+
+    console.log(jsondata)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
     </div>
   );
 }
